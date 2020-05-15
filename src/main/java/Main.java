111ded2de9c4a08d09522c89f8cbea9e05f05a05
Toooo123 <34826963+Toooo123@users.noreply.github.com>
2020-05-14 19:44:13 -0700

    Structure Folders and Model classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import camera.ui.CameraTextUI;
+
+/**
+ * Launch application
+ */
+public class Main {
+    public static void main(String args[]) {
+        LensManager manager = new LensManager();
+        CameraTextUI ui = new CameraTextUI(manager);
+        ui.show();
+    }
+}
