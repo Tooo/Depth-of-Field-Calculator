3b55ed12583bf398c344904944f1add7c1a57614
tsc7 <tsc7@sfu.ca>
2020-05-16 17:37:02 -0700

    Lens

--- src/main/java/camera/model/Lens.java
+++ src/main/java/camera/model/Lens.java
@@ -1,4 +1,14 @@
 package camera.model;
 
 public class Lens {
+    String make;
+    float  maxAperture;
+    int focalLength;
+
+    public Lens (String make, float maxAperture, int focalLength) {
+        this.make = make;
+        this.maxAperture = maxAperture;
+        this.focalLength = focalLength;
+    }
+
 }
