f805b007faef1a1211c83a6b4dcc966f25bfb1e3
tsc7 <tsc7@sfu.ca>
2020-05-16 19:52:36 -0700

    LensManager

--- src/main/java/camera/model/LensManager.java
+++ src/main/java/camera/model/LensManager.java
@@ -1,4 +1,18 @@
 package camera.model;
 
-public class LensManager {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class LensManager implements Iterable<Lens>{
+    private List<Lens> lens = new ArrayList<>();
+
+    public void add(Lens len) {
+        lens.add(len);
+    }
+
+    @Override
+    public Iterator<Lens> iterator() {
+        return lens.iterator();
+    }
 }
